c4f009639d914448a859b07e7afab85910a93640
Luno-front-end <Onlyrobochaposhta@gmail.com>
2023-04-11 18:59:37 +0300

    add server link

--- client/src/Api/getEmployees.ts
+++ client/src/Api/getEmployees.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080";
-// const BASE_URL = "http://195.54.175.196";
+const BASE_URL = "http://195.54.175.196";
 //
 // http://195.54.175.196/reviews
 
